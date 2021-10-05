c993a7db6dd7709633ec049fde0928dad7cb98a6
MelinaDCB <melina.decombas@gmail.com>
2021-10-05 14:29:11 +0200

    final-final-commit-i-swear

--- exo_15.rb
+++ exo_15.rb
@@ -2,7 +2,11 @@ puts "Bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print ">"
 floor_number = gets.chomp.to_i
 floor = 1
-while floor != floor_number + 1
-    puts "#" * floor
-    floor += 1
+if floor_number <= 25
+    while floor != floor_number + 1
+        puts "#" * floor
+        floor += 1
+    end
+else
+    puts "Le nombre entré est incorrect, merci de réessayer"
 end
--- exo_17.rb
+++ exo_17.rb
@@ -2,9 +2,13 @@ puts "Bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print ">"
 floor_number = gets.chomp.to_i
 floor = 1
-while floor != floor_number + 1
-    print " " * (floor_number - floor)
-    print "#" * floor
-    puts "#" * (floor - 1)
-    floor += 1
+if floor_number <= 25
+    while floor != floor_number + 1
+        print " " * (floor_number - floor)
+        print "#" * floor
+        puts "#" * (floor - 1)
+        floor += 1
+    end
+else
+    puts "Le nombre entré est incorrect, merci de réessayer"
 end
